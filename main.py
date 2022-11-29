c6ba9de3a2944afc1904187bd2b1dcd09f9ef0fd
WonDKim <kwd92@hotmail.com>
2022-11-29 21:31:43 +0900

    update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import math
 
 import cv2
 import numpy as np
-import torch
 import mujoco_py as mjp
+
+import torch
+from model.networks import LSTMUnet3dGenerator
+import torchvision.transforms as T
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 obj_list = ['circleshell', 'cone', 'cross', 'cubehole', 'cuboid', 'cylinder', 'doubleslope', 'hemisphere', 'line',
             'pacman', 'S', 'sphere', 'squareshell', 'star', 'tetrahedron', 'torus']
@@ -16,10 +21,9 @@ parser.add_argument('--object', '-obj', type=str, default='circleshell', choices
 parser.add_argument('--x_init', '-x', type=float, default=0.0)
 parser.add_argument('--y_init', '-y', type=float, default=0.0)
 parser.add_argument('--r_init', '-r', type=float, default=0.0)
-parser.add_argument('--dx', '-dx', type=float, default=0.5)
-parser.add_argument('--dy', '-dy', type=float, default=0.1)
-parser.add_argument('--dz', '-dz', type=float, default=0.1)
-parser.add_argument('--visualize_sim', '-vis_sim', type=bool, default=False)
+parser.add_argument('--dx', '-dx', type=float, default=0.652)
+parser.add_argument('--dy', '-dy', type=float, default=-0.3)
+parser.add_argument('--dz', '-dz', type=float, default=1.5)
 parser.add_argument('--visualize_motion_vector', '-vis_vec', type=bool, default=False)
 args = parser.parse_args()
 
@@ -36,13 +40,6 @@ def depth2img(depth, sim):
 
     return image.astype(np.uint8)
 
-def get_depth_img(sim):
-    _, depth_frame = sim.render(width=1640, height=1232, camera_name='tact_cam', depth=True)
-    depth_frame = cv2.flip(depth_frame, 0)
-    depth_img = depth2img(depth_frame, sim)
-
-    return crop_resize_depth_img(depth_img)
-
 def crop_resize_depth_img(img):
     offset = [-16, -16]
     img_center = [img.shape[0]/2 + offset[0], img.shape[1]/2 + offset[1]]
@@ -52,12 +49,40 @@ def crop_resize_depth_img(img):
 
     return cv2.merge((resized_img, resized_img, resized_img))
 
+def get_depth_img(sim):
+    _, depth_frame = sim.render(width=1640, height=1232, camera_name='tact_cam', depth=True)
+    depth_frame = cv2.flip(depth_frame, 0)
+    depth_img = depth2img(depth_frame, sim)
+    crop_resized_depth_img = crop_resize_depth_img(depth_img)
+
+    cv2.imshow('simulated depth', crop_resized_depth_img)
+    cv2.waitKey(1)
+
+    transforms = T.Compose([T.ToTensor(), T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    img_tensor = transforms(crop_resized_depth_img)
+
+    return img_tensor.view(1, img_tensor.shape[0], img_tensor.shape[1], img_tensor.shape[2])
+
 def add_to_sequence(img, seq):
-    return np.vstack((seq, img.reshape(1, 256, 256, 3)))
+    return torch.cat((seq, img))
 
 def set_sensor_pos(pos, sim):
     sim.data.qpos[:] = pos
     sim.forward()
+
+def seq_to_input(seq):
+    seq = torch.permute(seq, (1, 0, 2, 3))
+    seq = seq[None, :, :, :, :]
+    return seq.to(device)
+
+def output_to_img(output):
+    transform = T.Normalize((-1, -1, -1), (2, 2, 2))
+    img = output[0, :, -1, :, :]
+    img = transform(img)
+    img_np = img.detach().cpu().numpy()
+    img_np = np.transpose(img_np * 255, (1, 2, 0))
+    img = img_np.astype(np.uint8)
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
 
 if __name__ == '__main__':
@@ -79,8 +104,7 @@ if __name__ == '__main__':
     # set to initial position
     qpos_init = [obj_pos[0] + args.x_init * 0.001, obj_pos[1] + args.y_init * 0.001, 0., args.r_init * np.pi / 180]
     set_sensor_pos(qpos_init, sim)
-    sim_depth = get_depth_img(sim)
-    depth_seq = sim_depth.reshape(1, 256, 256, 3)
+    depth_seq = get_depth_img(sim)
 
     # move dz
     qpos = qpos_init[:]
@@ -96,16 +120,10 @@ if __name__ == '__main__':
         sim_depth = get_depth_img(sim)
         depth_seq = add_to_sequence(sim_depth, depth_seq)
 
-        if args.visualize_sim:
-            rgb_view_sim = sim.render(width=1920, height=1080, camera_name='cam', depth=False)
-            bgr_view_sim = cv2.flip(cv2.cvtColor(rgb_view_sim, cv2.COLOR_RGB2BGR), 0)
-
-            cv2.imshow("sim", bgr_view_sim)
-            cv2.waitKey(0)
-
     # move dx and/or dy
     dxy = math.sqrt(args.dx ** 2 + args.dy ** 2)
-    dxy_step = [0.0001 * args.dx / dxy, 0.0001 * args.dy / dxy]
+    if dxy != 0.:
+        dxy_step = [0.0001 * args.dx / dxy, 0.0001 * args.dy / dxy]
     dxy_cnt = math.ceil(dxy / 0.1)
     for i in range(dxy_cnt):
         if i < dxy_cnt - 1:
@@ -120,11 +138,16 @@ if __name__ == '__main__':
         sim_depth = get_depth_img(sim)
         depth_seq = add_to_sequence(sim_depth, depth_seq)
 
-        if args.visualize_sim:
-            rgb_view_sim = sim.render(width=1920, height=1080, camera_name='cam', depth=False)
-            bgr_view_sim = cv2.flip(cv2.cvtColor(rgb_view_sim, cv2.COLOR_RGB2BGR), 0)
+    # generate tactile image
+    G = LSTMUnet3dGenerator().to(device)
+    G.load_state_dict(torch.load(os.path.join('model', 'lstm_3d_unet.pt')))
+    G.eval()
 
-            cv2.imshow("sim", bgr_view_sim)
-            cv2.waitKey(0)
+    G_input = seq_to_input(depth_seq)
+    G_output = G(G_input)
+    gen_img = output_to_img(G_output)
+
+    cv2.imshow('generated image', gen_img)
+    cv2.waitKey(0)
 
     cv2.destroyAllWindows()
